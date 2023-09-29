e4144c7eccb62e1227e116a68601a0b7dff4d8fb
Kauancarlos3 <carlos_kauan33@hotmail.com>
2023-09-29 08:36:33 -0300

    logica, listar tarefas

--- app_lista_tarefas_private/tarefa_controller.php
+++ app_lista_tarefas_private/tarefa_controller.php
@@ -4,19 +4,21 @@
     require_once "./app_lista_tarefas_private/conexao.php";
     require_once "./app_lista_tarefas_private/tarefas.service.php";
 
-    echo '<pre>';
-    print_r($_POST);
-    echo '</pre>';
 
-    $tarefa = new Tarefa();
-    $tarefa->__set('tarefa', $_POST['tarefa']);
+    $acao =  isset($_GET['acao']) ? $_GET['acao'] : $acao;
 
-    $conexao = new Conexao();
+    if($acao == 'inserir'){ 
+        $tarefa = new Tarefa();
+        $tarefa->__set('tarefa', $_POST['tarefa']);
 
-    $tarefaservice = new TarefaService($conexao, $tarefa);
+        $conexao = new Conexao();
 
-    echo '<pre>';
-    print_r($tarefaservice);
-    echo '</pre>';
+        $tarefaservice = new TarefaService($conexao, $tarefa);
+        $tarefaservice->inserir();
+
+        header('Location: nova_tarefa.php?inclusao=1');
+    } else if($acao == 'recuperar'){
+        echo 'chegamos aqui!';
+    }
 
 ?>
--- app_lista_tarefas_private/tarefas.service.php
+++ app_lista_tarefas_private/tarefas.service.php
@@ -13,6 +13,11 @@
 
         public function inserir(){ //create
 
+            $query = 'INSERT INTO tb_tarefas(tarefa)values(:tarefa)';
+            $stmt = $this->conexao->prepare($query);
+            $stmt->bindValue(':tarefa', $this->tarefa->__get('tarefa'));
+            $stmt->execute();
+
         }
         public function recuperar(){ //read
 
